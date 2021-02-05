2626a57493bf49c7758fc0c9ebfe9ef65138b476
Josh Jameson <josh@servebyte.com>
2021-02-05 18:33:15 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const path = require('path');
+var Server = require('bittorrent-tracker').Server
+
+var server = new Server({
+  udp: false, // enable udp server? [default=true]
+  http: true, // enable http server? [default=true]
+  ws: false, // enable websocket server? [default=true]
+  stats: false, // enable web-based statistics? [default=true]
+  trustProxy: true,
+  filter: function (infoHash, params, cb) {
+	var hash = path.basename(infoHash).toUpperCase();
+    if (hash.length==40 && fs.existsSync('/tracker/'+hash)) {
+      // If the callback is passed `null`, the torrent will be allowed.
+      cb(null)
+    } else {
+      // If the callback is passed an `Error` object, the torrent will be disallowed
+      // and the error's `message` property will be given as the reason.
+      cb(new Error('disallowed torrent'))
+    }
+  }
+})
+
+server.on('error', function (err) {
+  // fatal server error!
+  console.log(err.message)
+})
+
+/*
+server.on('warning', function (err) {
+  // client sent bad data. probably not a problem, just a buggy client.
+  console.log(err.message)
+})
+*/
+
+server.on('listening', function () {
+  console.log('listening on http port:' + server.http.address().port)
+})
+
+server.listen(80, '0.0.0.0')
